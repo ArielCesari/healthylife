1c2d35565a5b6874b7847dcf92a812af2fe1011e
ArielCesari <ariel.dcesari@gmail.com>
2023-05-27 18:28:15 -0300

    Agrego cambios en scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -5,8 +5,25 @@ document.getElementById("button-up").addEventListener("click", scrollUp);
 function scrollUp() {
 
     let currentScroll = document.documentElement.scrollTop;
-
+    
     if (currentScroll > 0) {
-        window.scrollTo(0, 0);
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0, currentScroll - (currentScroll / 10));
+    }
+}
+
+
+// Hacer que el boton solo aparezca cuando no estamos en el top 
+
+let buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    let scroll = document.documentElement.scrollTop;
+
+    if(scroll > 1100 ) {
+       buttonUp.style.transform = "scale(1)";
+    } else if (scroll < 500){
+        buttonUp.style.transform = "scale(0)";
     }
 }
