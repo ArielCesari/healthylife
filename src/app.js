29b6610dd9af93a3671e10c20cff94f252c46c63
ArielCesari <ariel.dcesari@gmail.com>
2023-08-17 11:32:15 -0300

    Cambios de prueba

--- src/app.js
+++ src/app.js
@@ -8,20 +8,14 @@ const app = express();
 /* Importo los enrutadores */
 const mainRouter = require('./routes/mainRouter.js');
 
-
-
 /* Archivos estáticos para buscar archivos en carpeta public */ 
 app.use(express.static("public"));
-
 
 /* Usamos los enrutadores importados */
 app.use("/",mainRouter);
 app.use("/consejos",mainRouter);
 app.use("/motivaciones",mainRouter);
 app.use("/recetas",mainRouter);
-
-
-
 
 
 const port = process.env.PORT || 3000;
